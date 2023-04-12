3291dac85073726bb7c90edfc515a7ec1c65180f
valsap1 <94027080+valsap1@users.noreply.github.com>
2023-04-11 18:19:31 -0600

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6435f8e33dedc4001fca9d17",
+				"6435f8e33dedc4001fca9d1a"
+			]
+		},
+		"6435f8e33dedc4001fca9d17": {
+			"id": "6435f8e33dedc4001fca9d17",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6435f8e33dedc4001fca9d1a": {
+			"id": "6435f8e33dedc4001fca9d1a",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
